47880d887a3693cf0433342c913cc787f8b1ec01
Arjav Sharma <arjav.sharma.91@gmail.com>
2026-06-05 15:51:30 -0400

    Update rounding.py
    Bug Fixes and Testing Validation for add, sub, mult

--- rounding.py
+++ rounding.py
@@ -1,48 +1,62 @@
-from gmpy2 import RoundDown, RoundUp, local_context
+from gmpy2 import RoundDown, RoundUp, context, get_context
 
 def add_down(a, b):
-  with local_context() as ctx:
-    ctx.round = RoundDown
-    return a+b
+    a = Number(a)
+    b = Number(b)
+    with context(get_context()) as ctx:
+        ctx.round = RoundDown
+        return a+b
 
 def add_up(a, b):
-  with local_context() as ctx:
-    ctx.round = RoundUp
-    return a+b
+    a = Number(a)
+    b = Number(b)
+    with context(get_context()) as ctx:
+        ctx.round = RoundUp
+        return a+b
 
 def sub_down(a, b):
-  with local_context() as ctx:
-    ctx.round = RoundDown
-    return a-b
+    a = Number(a)
+    b = Number(b)
+    with context(get_context()) as ctx:
+        ctx.round = RoundDown
+        return a-b
 
 def sub_up(a, b):
-  with local_context() as ctx:
-    ctx.round = RoundUp
-    return a-b
+    a = Number(a)
+    b = Number(b)
+    with context(get_context()) as ctx:
+        ctx.round = RoundUp
+        return a-b
 
 def mul_down(a, b):
-  with local_context() as ctx:
-    ctx.round = RoundDown
-    return a*b
+    a = Number(a)
+    b = Number(b)
+    with context(get_context()) as ctx:
+        ctx.round = RoundDown
+        return a*b
 
 def mul_up(a, b):
-  with local_context() as ctx:
-    ctx.round = RoundUp
-    return a*b
+    a = Number(a)
+    b = Number(b)
+    with context(get_context()) as ctx:
+        ctx.round = RoundUp
+        return a*b
 
 # IMPORTANT, DIVISION IS WAY MORE COMPLEX THAN THIS, THIS IS JUST A BASE UNDERSTANDING
 
 def div_down(a, b):
-  with local_context() as ctx:
-    ctx.round = RoundDown
-    return a / b
+    a = Number(a)
+    b = Number(b)
+    with context(get_context()) as ctx:
+        ctx.round = RoundDown
+        return a / b
 
 def div_up(a, b):
-  with local_context() as ctx:
-    ctx.round = RoundUp
-    return a / b
-
-
+    a = Number(a)
+    b = Number(b)
+    with context(get_context()) as ctx:
+        ctx.round = RoundUp
+        return a / b
 
 
 
