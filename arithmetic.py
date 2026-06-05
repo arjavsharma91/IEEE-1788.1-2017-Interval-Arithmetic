5008e4632671808fe1c7a03f2c5bce83d76e7b8b
Arjav Sharma <arjav.sharma.91@gmail.com>
2026-06-05 00:42:11 -0400

    Create arithmetic.py
    store main ops here

---
+++ arithmetic.py
@@ -0,0 +1,9 @@
+from .interval import Interval
+from .rounding import add_down, add_up, sub_down, sub_up, div_down, div_up, mult_down, mult_up
+
+# Arithmetic layer rule:
+# never modify Interval class
+# never implement rounding here
+# only combine endpoints using rounding.py
+# always return Interval
+
