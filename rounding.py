d1d59025ef2aff4747c14785882d8fef2e4e9000
Arjav Sharma <arjav.sharma.91@gmail.com>
2026-06-04 23:46:28 -0400

    Create rounding.py
    basic operations with rounding and such, really important for floating point stuff

---
+++ rounding.py
@@ -0,0 +1,49 @@
+from gmpy2 import RoundDown, RoundUp, local_context
+
+def add_down(a, b):
+  with local_context() as ctx:
+    ctx.round = RoundDown
+    return a+b
+
+def add_up(a, b):
+  with local_context() as ctx:
+    ctx.round = RoundUp
+    return a+b
+
+def sub_down(a, b):
+  with local_context() as ctx:
+    ctx.round = RoundDown
+    return a-b
+
+def sub_up(a, b):
+  with local_context() as ctx:
+    ctx.round = RoundUp
+    return a-b
+
+def mult_down(a, b):
+  with local_context() as ctx:
+    ctx.round = RoundDown
+    return a*b
+
+def mult_up(a, b):
+  with local_context() as ctx:
+    ctx.round = RoundUp
+    return a*b
+
+# IMPORTANT, DIVISION IS WAY MORE COMPLEX THAN THIS, THIS IS JUST A BASE UNDERSTANDING
+
+def div_down(a, b):
+  with local_context() as ctx:
+    ctx.round = RoundDown
+    return a / b
+
+def div_up(a, b):
+  with local_context() as ctx:
+    ctx.round = RoundUp
+    return a / b
+
+
+
+
+
+  
