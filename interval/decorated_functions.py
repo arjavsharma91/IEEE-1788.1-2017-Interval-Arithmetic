a608ff0fcda4b2ac354cae6cf93f35ae8f1c34c3
Arjav Sharma <arjav.sharma.91@gmail.com>
2026-06-24 12:19:19 -0400

    Update decorated_functions.py
    log, sqrt

--- interval/decorated_functions.py
+++ interval/decorated_functions.py
@@ -5,8 +5,41 @@ from .decorations import Decoration, combine
 def exp(x):
   x = DecoratedInterval._coerce(x)
   if x.is_nai:
-    return DecoratedInterval.nai
+    return DecoratedInterval.new_nai()
   interval = bare_exp(x.interval)
   dec = combine(x.decoration)
   return DecoratedInterval(interval, dec)
 
+def sqrt(x):
+  x = DecoratedInterval._coerce(x)
+  if x.is_nai:
+    return DecoratedInterval.new_nai()
+  if x.interval.hi < 0:
+    return DecoratedInterval.empty()
+  elif x.interval.lo < 0:
+    op_dec = Decoration.TRV
+  else:
+    op_dec = Decoration.COM
+  interval = bare_sqrt(x.interval)
+  dec = combine(x.decoration, op_dec)
+
+  if dec == Decoration.COM and not interval.is_bounded:
+    dec = Decoration.DAC
+
+  return DecoratedInterval(interval, dec)
+
+def log(x):
+  x = DecoratedInterval._coerce(x)
+  if x.is_nai:
+    return DecoratedInterval.new_nai()
+  if x.interval.hi < 0:
+    return DecoratedInterval.empty()
+  elif x.interval.lo < 0:
+    op_dec = Decoration.TRV
+  else:
+    op_dec = Decoration.COM
+  interval = bare_log(x.interval)
+  dec = combine(x.decoration, op_dec)
+  if dec == Decoration.COM and not interval.is_bounded:
+    dec = Decoration.DAC
+  return DecoratedInterval(interval, dec)
